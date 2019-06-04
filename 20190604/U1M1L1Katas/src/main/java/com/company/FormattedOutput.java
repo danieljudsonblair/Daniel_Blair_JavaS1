46a1af70fadc48c571d94ae18d120813c9ef6156
s1191 <danieljudsonblair@gmail.com>
2019-06-03 21:16:39 -0400

    first homework kata 06042019

---
+++ 20190604/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class FormattedOutput {
+    public static void main(String[] args) {
+        System.out.printf("My name is %s and I am %d years old", "Dan", 40);
+    }
+}
