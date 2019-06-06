00bed4a7927398369299f117223455565b4eea3c
s1191 <danieljudsonblair@gmail.com>
2019-06-06 01:06:59 -0400

    var refactor

--- 20190606/FizzBuzz/src/main/java/com/company/FizzBuzz.java
+++ 20190606/FizzBuzz/src/main/java/com/company/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        for (byte i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
                 System.out.println("FIZZBUZZ");
             } else if (i % 5 == 0) {
