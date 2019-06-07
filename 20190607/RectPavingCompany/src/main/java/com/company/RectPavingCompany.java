3e59c9f612da7fa487048aaf4b983d9587c4303b
s1191 <danieljudsonblair@gmail.com>
2019-06-06 23:34:37 -0400

    updated comments

--- 20190607/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
+++ 20190607/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class RectPavingCompany {
 
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat("##.##");  // Added to format decimals
-        df.setRoundingMode(RoundingMode.DOWN);                  // Added to format decimals
+        DecimalFormat df = new DecimalFormat("##.##");     // Added to format decimals
+        df.setRoundingMode(RoundingMode.DOWN);                     // Added to round decimals
         Scanner scan = new Scanner(System.in);
 
         System.out.print("What is the width of the driveway in feet? ");
-        float width = Float.parseFloat(scan.nextLine());  // Changed type to float and used parseFloat method
+        float width = Float.parseFloat(scan.nextLine());           // Changed type to float and use parseFloat method
 
         System.out.print("What is the length of the driveway in feet? ");
-        float length = Float.parseFloat(scan.nextLine()); // Changed type to float and used parseFloat method
+        float length = Float.parseFloat(scan.nextLine());          // Changed type to float and use parseFloat method
 
         float cementCost = 0.0f;
         float framingCost = 0.0f;
@@ -24,15 +24,15 @@ public class RectPavingCompany {
         cementCost = Float.parseFloat(scan.nextLine());
 
         System.out.print("What is the cost of framing/footers per linear foot? ");
-        framingCost = Float.parseFloat(scan.nextLine());  //  Changed to framingCost
+        framingCost = Float.parseFloat(scan.nextLine());           //  Changed cementCost to framingCost
 
-        float area = length * width;                // Changed to type float
-        float perimeter = 2 * length + 2 * width;   // Changed to type float, fixed calculation
+        float area = length * width;                               // Changed to type float
+        float perimeter = 2 * length + 2 * width;                  // Changed to type float, changed operator from - to +
 
-        System.out.format("The area of the driveway is " + df.format(area) + " square feet. %n");   // Changed format
-        System.out.format("The perimeter of the driveway is " + df.format(perimeter) + " feet. %n"); // Changed format & suffix
+        System.out.format("The area of the driveway is " + df.format(area) + " square feet. %n");    // Changed display format
+        System.out.format("The perimeter of the driveway is " + df.format(perimeter) + " feet. %n"); // Changed display format & suffix
 
-        System.out.format("The cost of the cement is $" + df.format(area * cementCost) + "%n");  // Changed format
-        System.out.format("The cost of the framing/footers is $" + df.format(perimeter * framingCost) + "%n"); // Changed format and calc
+        System.out.format("The cost of the cement is $" + df.format(area * cementCost) + "%n");                // Changed display format
+        System.out.format("The cost of the framing/footers is $" + df.format(perimeter * framingCost) + "%n"); // Changed display format and calculation
     }
 }
