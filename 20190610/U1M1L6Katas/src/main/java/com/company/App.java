38687668d3d443a516c3107a032a2ea70f796cb8
s1191 <danieljudsonblair@gmail.com>
2019-06-07 23:37:51 -0400

    added outputs to console on all methods

--- 20190610/U1M1L6Katas/src/main/java/com/company/App.java
+++ 20190610/U1M1L6Katas/src/main/java/com/company/App.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class App {
+
     public static int total(int[] arr) {
         int total = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -9,6 +12,7 @@ public class App {
         }
         return total;
     }
+
 
     public static int totalOdd(int[] arr) {
         int total = 0;
@@ -20,6 +24,7 @@ public class App {
         return total;
     }
 
+
     public static int totalEven(int[] arr) {
         int total = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -29,6 +34,7 @@ public class App {
         }
         return total;
     }
+
 
     public static int secondLargestNumber(int[] arr) {
         int first = arr[0];
@@ -45,6 +51,7 @@ public class App {
         return second;
     }
 
+
     public static int[] reverse(int[] arr) {
         int[] arr2 = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
@@ -53,12 +60,14 @@ public class App {
         return arr2;
     }
 
+
     public static String[] swapFirstAndLast(String[] arr) {
         String holder = arr[0];
         arr[0] = arr[arr.length - 1];
         arr[arr.length - 1] = holder;
         return arr;
     }
+
 
     public static String concatenateString(String[] arr) {
         String str = arr[0];
@@ -67,6 +76,7 @@ public class App {
         }
         return str;
     }
+
 
     public static int[] everyThird(int[] arr) {
         int[] arr2 = new int[arr.length / 3];
@@ -79,6 +89,7 @@ public class App {
             return arr2;
         }
     }
+
 
     public static int[] lessThanFive(int[] arr) {
         int ctr = 0;
@@ -129,6 +140,7 @@ public class App {
         return arr2;
     }
 
+
     public static String[][] evensAndOdds(String[] arr) {
         int evens = 0;
         int odds = 0;
@@ -155,9 +167,21 @@ public class App {
         return arr2;
     }
 
+
     public static void main(String[] args) {
-        int arr[] = {-17, -3};
+        int[] arr = {0,1,2,3,4,5,6,7};
+        String[] strArr = {"foo", "bar", "baz", "qux"};
+        System.out.println(total(arr));
+        System.out.println(totalOdd(arr));
+        System.out.println(totalEven(arr));
         System.out.println(secondLargestNumber(arr));
+        System.out.println(Arrays.toString(swapFirstAndLast(strArr)));
+        System.out.println(Arrays.toString(reverse(arr)));
+        System.out.println(concatenateString(strArr));
+        System.out.println(Arrays.toString(everyThird(arr)));
+        System.out.println(Arrays.toString(lessThanFive(arr)));
+        System.out.println(Arrays.deepToString(splitAtFive(arr)));
+        System.out.println(Arrays.deepToString(evensAndOdds(strArr)));
     }
 }
 
