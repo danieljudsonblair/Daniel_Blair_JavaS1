f07c7b4c3a307a6fbbc2b53f717c16ca1e3bab3b
s1191 <danieljudsonblair@gmail.com>
2019-06-18 08:05:49 -0400

    classmatelist fix

--- 20190618/U1M3L1Katas/src/main/java/com/company/ClassmatesList.java
+++ 20190618/U1M3L1Katas/src/main/java/com/company/ClassmatesList.java
@@ -1,16 +1,13 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ClassmatesList {
     private ArrayList<Classmate> classmatesList = new ArrayList<>();
 
 
     public void add(Classmate classmate) {
-        classmate.setName(classmate.getName());
-        classmate.setHairColor(classmate.getHairColor());
+        classmatesList.add(classmate);
     }
 
     public Classmate get(int num) {
@@ -20,27 +17,4 @@ public class ClassmatesList {
     public ArrayList<Classmate> getAll() {
         return classmatesList;
     }
-
-    public static void main(String[] args) {
-        Classmate one = new Classmate();
-        Classmate two = new Classmate();
-        Classmate three = new Classmate();
-
-        one.setName("Dan");
-        one.setHairColor("Blonde");
-        two.setName("Charity");
-        two.setHairColor("Dark");
-        three.setName("Bob");
-        three.setHairColor("Black");
-
-        ArrayList<Classmate> classmate = new ArrayList<>();
-
-        classmate.add(one);
-        classmate.add(two);
-        classmate.add(three);
-        System.out.println(classmate.get(0).getName());
-
-
-    }
-
 }
