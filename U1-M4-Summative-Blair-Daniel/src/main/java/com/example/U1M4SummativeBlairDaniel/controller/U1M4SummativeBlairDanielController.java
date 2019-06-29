1770996b82f4f6797a9bf371e9992dc20457c7a8
s1191 <danieljudsonblair@gmail.com>
2019-06-28 23:02:47 -0400

    initial commit

---
+++ U1-M4-Summative-Blair-Daniel/src/main/java/com/example/U1M4SummativeBlairDaniel/controller/U1M4SummativeBlairDanielController.java
@@ -0,0 +1,152 @@
+package com.example.U1M4SummativeBlairDaniel.controller;
+
+import com.example.U1M4SummativeBlairDaniel.model.Answer;
+import com.example.U1M4SummativeBlairDaniel.model.Definition;
+import com.example.U1M4SummativeBlairDaniel.model.Quote;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+public class U1M4SummativeBlairDanielController {
+
+    @RequestMapping(value = "/quote", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Quote getQuote() {
+        Quote quote = new Quote();
+        Random rand = new Random();
+
+        int num = rand.nextInt((9) + 1) + 1;
+        switch (num) {
+            case 1:
+                quote.setAuthor("Pee-Wee Herman");
+                quote.setQuote("There's a lot about me you don't understand.  Things you wouldn't understand, things you couldn't understand, things you SHOULDN'T understand.");
+                return quote;
+            case 2:
+                quote.setAuthor("Eugene Robinson");
+                quote.setQuote("Run the length of the football field; stiff-arm a man in dey mowf.");
+                return quote;
+            case 3:
+                quote.setAuthor("George Washington");
+                quote.setQuote("I cannot tell a lie.");
+                return quote;
+            case 4:
+                quote.setAuthor("Darth Vader");
+                quote.setQuote("No, I am your Father.");
+                return quote;
+            case 5:
+                quote.setAuthor("Maury Povich");
+                quote.setQuote("You are NOT the father.");
+                return quote;
+            case 6:
+                quote.setAuthor("Daniel Blair");
+                quote.setQuote("My name is Dan.  People call me Dan.");
+                return quote;
+            case 7:
+                quote.setAuthor("Trilogy Curriculum");
+                quote.setQuote("Your service must contain at least 10 quotes.");
+                return quote;
+            case 8:
+                quote.setAuthor("Matt Foley");
+                quote.setQuote("... and I live in a VAN DOWN BY THE RIVER.");
+                return quote;
+            case 9:
+                quote.setAuthor("Neil Armstrong");
+                quote.setQuote("That's one small step for (a) man, one giant leap for mankind.");
+                return quote;
+            case 10:
+                quote.setAuthor("Scott Manley");
+                quote.setQuote("I'm Scott Manley.  Fly Safe.");
+                return quote;
+            default:
+                return null;
+        }
+    }
+
+    @RequestMapping(value = "/word", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Definition getDefinition() {
+        Definition definition = new Definition();
+        Random rand = new Random();
+
+        int num = rand.nextInt((9) + 1) + 1;
+        switch (num) {
+            case 1:
+                definition.setWord("brochette");
+                definition.setDefinition("skewer for holding food steady while cooking");
+                return definition;
+            case 2:
+                definition.setWord("urdee");
+                definition.setDefinition("of a heraldic device, pointed or broken into parallel pointed projections");
+                return definition;
+            case 3:
+                definition.setWord("stillatim");
+                definition.setDefinition("drop by drop");
+                return definition;
+            case 4:
+                definition.setWord("umbiliciform");
+                definition.setDefinition("shaped like a navel");
+                return definition;
+            case 5:
+                definition.setWord("croche");
+                definition.setDefinition("knob at the top of a deer's horn");
+                return definition;
+            case 6:
+                definition.setWord("subfocal");
+                definition.setDefinition("of which one is only dimly aware");
+                return definition;
+            case 7:
+                definition.setWord("landau");
+                definition.setDefinition("horse-drawn carriage with folding top");
+                return definition;
+            case 8:
+                definition.setWord("onymous");
+                definition.setDefinition("bearing the author's name");
+                return definition;
+            case 9:
+                definition.setWord("hula");
+                definition.setDefinition("sinuous Polynesian dance with rhythmic hip movements");
+                return definition;
+            case 10:
+                definition.setWord("bisulcate");
+                definition.setDefinition("cloven-footed; cleft in two");
+                return definition;
+            default:
+                return null;
+        }
+    }
+
+    @RequestMapping(value = "/magic", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Answer postQuestion(@RequestBody Answer answerIn) {
+        Answer answer = new Answer();
+        answer.setQuestion(answerIn.getQuestion());
+        Random rand = new Random();
+
+        int num = rand.nextInt((5) + 1) + 1;
+
+        switch (num) {
+            case 1:
+                answer.setAnswer("It is certain");
+                return answer;
+            case 2:
+                answer.setAnswer("Yes");
+                return answer;
+            case 3:
+                answer.setAnswer("Ask again later");
+                return answer;
+            case 4:
+                answer.setAnswer("Don't count on it");
+                return answer;
+            case 5:
+                answer.setAnswer("Very doubtful");
+                return answer;
+            case 6:
+                answer.setAnswer("Reply hazy, try again");
+                return answer;
+            default:
+                return null;
+        }
+    }
+}
---
+++ U1-M4-Summative-Blair-Daniel/src/main/java/com/example/U1M4SummativeBlairDaniel/model/Definition.java
@@ -0,0 +1,22 @@
+package com.example.U1M4SummativeBlairDaniel.model;
+
+public class Definition {
+    private String word;
+    private String definition;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
