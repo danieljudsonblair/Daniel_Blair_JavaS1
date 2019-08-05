037184360c5504e7a1ab653fdec648a4d438ce4b
s1191 <danieljudsonblair@gmail.com>
2019-08-04 22:07:01 -0400

    correct adservice

--- DanielBlairU2M1Summative/adserver-service/src/main/java/com/trilogyed/adserver/controller/AdserverController.java
+++ DanielBlairU2M1Summative/adserver-service/src/main/java/com/trilogyed/adserver/controller/AdserverController.java
@@ -1,5 +1,6 @@
 package com.trilogyed.adserver.controller;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
