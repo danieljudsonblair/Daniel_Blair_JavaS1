896261b8952151cb1c48a286496e018604b373de
s1191 <danieljudsonblair@gmail.com>
2019-08-30 15:36:49 -0400

    sync

--- Interview Pair Project/banking-service/src/main/java/com/example/bankingservice/controller/BankingController.java
+++ Interview Pair Project/banking-service/src/main/java/com/example/bankingservice/controller/BankingController.java
@@ -3,10 +3,7 @@ package com.example.bankingservice.controller;
 import com.example.bankingservice.dao.AccountDao;
 import com.example.bankingservice.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +14,13 @@ public class BankingController {
     @Autowired
     AccountDao dao;
 
+    @PostMapping
+    public Account addAccount(@RequestBody Account account) {
+        return dao.addAccount(account);
+    }
+
     @GetMapping("/{accountNum}")
     public List<Account> getAccountsByAccountNum(@PathVariable String accountNum) {
-        return null;
+        return dao.getAllByAccountNum(accountNum);
     }
 }
